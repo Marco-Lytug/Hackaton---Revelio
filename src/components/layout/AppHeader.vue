0ee779ad10e0a7ef76d485941e0ec8c7ce37bdf5
Marco-Lytug <marcoanthonyo.hy@gmail.com>
2026-06-10 13:28:36 -0300

    FEAT: Criação do AppHeader

---
+++ src/components/layout/AppHeader.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+
+</script>
+
+<template>
+  <header>
+    <nav>
+      <ul>
+
+      <RouterLink to="/">Principa</RouterLink>
+      <RouterLink to="/livros">Livros</RouterLink>
+       <img src="/public/images/Revelio.png" alt="">
+      <RouterLink to="/atividades">Atividades</RouterLink>
+      <RouterLink to="/extras">Extras</RouterLink>
+      </ul>
+    </nav>
+  </header>
+
+</template>
+
+
+<style scoped>
+  img {
+    width: 65px;
+    height: auto;
+    position: relative;
+  }
+  ul {
+    display: flex;
+    list-style: none;
+    padding: 0;
+    margin: 0;
+  }
+</style>
