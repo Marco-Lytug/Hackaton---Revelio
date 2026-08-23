5291699bb82c22bc9a9d17e4cd794db3bd259917
DaviMariano123 <masterday0000@gmail.com>
2026-08-23 17:17:49 -0300

    FEAT: sobre nos -- style feito

--- src/views/SobreNos.vue
+++ src/views/SobreNos.vue
@@ -220,3 +220,324 @@
 
   </div>
 </template>
+
+
+<style scoped>
+
+.sobre {
+  width: 100%;
+  padding: 140px 8vw 100px;
+
+  background-color: #ffffff;
+
+  color: #141c21;
+
+  font-family: Arial, sans-serif;
+}
+
+
+.titulo {
+  max-width: 1100px;
+  margin: 0 auto 100px;
+}
+
+.titulo h1 {
+  font-size: 60px;
+  font-weight: normal;
+  margin-bottom: 20px;
+}
+
+.titulo p {
+  font-size: 28px;
+}
+
+
+/* EQUIPES */
+
+.equipe {
+  max-width: 1100px;
+  margin: 0 auto 120px;
+}
+
+.equipe h2 {
+  font-size: 42px;
+  font-weight: normal;
+
+  margin-bottom: 60px;
+}
+
+
+/* PESSOAS */
+
+.pessoas {
+  display: grid;
+
+  grid-template-columns: 1fr 1fr;
+
+  gap: 80px 60px;
+}
+
+.pessoa {
+  text-align: center;
+}
+
+.pessoa img,
+.sem-foto {
+  width: 100%;
+  height: 450px;
+
+  object-fit: cover;
+
+  border-radius: 35px;
+}
+
+
+/* FOTO QUE NÃO EXISTE */
+
+.sem-foto {
+  background-color: #e8e8e8;
+
+  display: flex;
+
+  align-items: center;
+  justify-content: center;
+}
+
+.sem-foto p {
+  font-size: 22px;
+  color: #777;
+}
+
+
+.pessoa h3 {
+  margin-top: 20px;
+
+  font-family: Georgia, serif;
+
+  font-size: 27px;
+
+  font-weight: normal;
+
+  line-height: 1.2;
+}
+
+
+/* SOBRE A REVELIO */
+
+.sobre-revelio {
+  max-width: 1200px;
+
+  margin: 0 auto;
+
+  padding: 70px;
+
+  background-color: #F4E6CC;
+
+  border: 5px solid #135F7D;
+
+  border-radius: 50px;
+}
+
+
+.sobre-revelio > h2 {
+  font-size: 45px;
+
+  font-weight: normal;
+
+  margin-bottom: 30px;
+}
+
+
+.descricao {
+  font-size: 23px;
+
+  line-height: 1.7;
+
+  color: #444;
+
+  margin-bottom: 60px;
+}
+
+
+/* OBJETIVO */
+
+.objetivo {
+
+  background-color: white;
+
+  border: 2px solid #135F7D;
+
+  border-radius: 30px;
+
+  padding: 35px;
+
+  margin-bottom: 60px;
+
+}
+
+
+.objetivo h3 {
+
+  color: #135F7D;
+
+  font-size: 28px;
+
+  margin-top: 0;
+
+}
+
+
+.objetivo p {
+
+  font-size: 21px;
+
+  line-height: 1.7;
+
+}
+
+
+/* CARDS */
+
+.cards {
+
+  display: grid;
+
+  grid-template-columns: repeat(3, 1fr);
+
+  gap: 30px;
+
+  margin-bottom: 80px;
+
+}
+
+
+.card {
+
+  border: 2px solid #135F7D;
+
+  border-radius: 30px;
+
+  padding: 30px;
+
+  min-height: 320px;
+
+}
+
+
+.card h3 {
+
+  color: #135F7D;
+
+  font-size: 25px;
+
+}
+
+
+.card p {
+
+  font-size: 19px;
+
+  line-height: 1.7;
+
+}
+
+
+/* DESENVOLVIMENTO */
+
+.desenvolvimento {
+
+  margin-bottom: 60px;
+
+}
+
+
+.desenvolvimento h2 {
+
+  font-size: 35px;
+
+  font-weight: normal;
+
+}
+
+
+.desenvolvimento p {
+
+  font-size: 21px;
+
+  line-height: 1.7;
+
+}
+
+
+/* FINAL */
+
+.final {
+
+  background-color: #135F7D;
+
+  color: white;
+
+  border-radius: 30px;
+
+  padding: 35px;
+
+}
+
+
+.final p {
+
+  font-size: 24px;
+
+  line-height: 1.6;
+
+  text-align: center;
+
+  margin: 0;
+
+}
+
+
+@media (max-width: 800px) {
+
+  .sobre {
+    padding: 120px 25px 70px;
+  }
+
+
+  .titulo h1 {
+    font-size: 42px;
+  }
+
+
+  .titulo p {
+    font-size: 22px;
+  }
+
+
+  .equipe h2 {
+    font-size: 32px;
+  }
+
+
+  .pessoas {
+    grid-template-columns: 1fr;
+  }
+
+
+  .sobre-revelio {
+    padding: 35px 25px;
+  }
+
+
+  .sobre-revelio > h2 {
+    font-size: 32px;
+  }
+
+
+  .cards {
+    grid-template-columns: 1fr;
+  }
+
+}
+
+</style>
