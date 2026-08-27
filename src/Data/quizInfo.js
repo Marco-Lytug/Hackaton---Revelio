4fcc3647e1611f03b633ee89c3db669a7d14bdd4
camilamengarda <camila.machado.mg@gmail.com>
2026-08-27 08:13:05 -0300

    Estrutura dos quizzes alterada e organizada

--- src/Data/quizInfo.js
+++ src/Data/quizInfo.js
@@ -1,11 +1,468 @@
-const quizInfo1 = []
-const quizInfo2 = []
-const quizInfo3 = []
 
-export { quizInfo1, quizInfo2, quizInfo3 }
+export const quizzesInfo = [
 
-export default {
-  quizInfo1,
-  quizInfo2,
-  quizInfo3
-}
+  // ================= 1º ANO =================
+  {
+    ano: 1,
+    nome: "1º Ano de Informática",
+    quizzes: [
+      // -----QUIZ 1-----
+      {
+        id: "1-quiz-1",
+        titulo: "",
+        perguntas: [
+          {
+            id: 1,
+            nivel: "Fácil",
+            pergunta: "",
+            alternativas: ["A)", "B)", "C)", "D)"],
+            respostaCorreta: 1
+          },
+          {
+            id: 2,
+            nivel: "Fácil",
+            pergunta: "",
+            alternativas: ["A)", "B)", "C)", "D)"],
+            respostaCorreta: 1
+          },
+          {
+            id: 3,
+            nivel: "Médio",
+            pergunta: "",
+            alternativas: ["A)",  "B)", "C)", "D)"],
+            respostaCorreta: 1
+          },
+          {
+            id: 4,
+            nivel: "Médio",
+            pergunta: "",
+            alternativas: ["A)",  "B)", "C)", "D)"],
+            respostaCorreta: 1
+          },
+          {
+            id: 5,
+            nivel: "Difícil",
+            pergunta: "",
+            alternativas: ["A)", "B)", "C)", "D)"],
+            respostaCorreta: 1
+          },
+          {
+            id: 6,
+            nivel: "Difícil",
+            pergunta: "",
+            alternativas: ["A)", "B)", "C)", "D)"],
+            respostaCorreta: 1
+          }
+        ]
+      },
+
+      // -----QUIZ 2-----
+      {
+        id: "1-quiz-2",
+        titulo: "",
+        perguntas: [
+          {
+            id: 1,
+            nivel: "Fácil",
+            pergunta: "",
+            alternativas: ["A)", "B)", "C)", "D)"],
+            respostaCorreta: 1
+          },
+          {
+            id: 2,
+            nivel: "Fácil",
+            pergunta: "",
+            alternativas: ["A)", "B)", "C)", "D)"],
+            respostaCorreta: 1
+          },
+          {
+            id: 3,
+            nivel: "Médio",
+            pergunta: "",
+            alternativas: ["A)",  "B)", "C)", "D)"],
+            respostaCorreta: 1
+          },
+          {
+            id: 4,
+            nivel: "Médio",
+            pergunta: "",
+            alternativas: ["A)",  "B)", "C)", "D)"],
+            respostaCorreta: 1
+          },
+          {
+            id: 5,
+            nivel: "Difícil",
+            pergunta: "",
+            alternativas: ["A)", "B)", "C)", "D)"],
+            respostaCorreta: 1
+          },
+          {
+            id: 6,
+            nivel: "Difícil",
+            pergunta: "",
+            alternativas: ["A)", "B)", "C)", "D)"],
+            respostaCorreta: 1
+          }
+        ]
+      },
+
+      // -----QUIZ 3-----
+      {
+        id: "1-quiz-3",
+        titulo: "",
+        perguntas: [
+          {
+            id: 1,
+            nivel: "Fácil",
+            pergunta: "",
+            alternativas: ["A)", "B)", "C)", "D)"],
+            respostaCorreta: 1
+          },
+          {
+            id: 2,
+            nivel: "Fácil",
+            pergunta: "",
+            alternativas: ["A)", "B)", "C)", "D)"],
+            respostaCorreta: 1
+          },
+          {
+            id: 3,
+            nivel: "Médio",
+            pergunta: "",
+            alternativas: ["A)",  "B)", "C)", "D)"],
+            respostaCorreta: 1
+          },
+          {
+            id: 4,
+            nivel: "Médio",
+            pergunta: "",
+            alternativas: ["A)",  "B)", "C)", "D)"],
+            respostaCorreta: 1
+          },
+          {
+            id: 5,
+            nivel: "Difícil",
+            pergunta: "",
+            alternativas: ["A)", "B)", "C)", "D)"],
+            respostaCorreta: 1
+          },
+          {
+            id: 6,
+            nivel: "Difícil",
+            pergunta: "",
+            alternativas: ["A)", "B)", "C)", "D)"],
+            respostaCorreta: 1
+          }
+        ]
+      }
+    ]
+  },
+
+  // ================= 2º ANO =================
+  {
+    ano: 2,
+    nome: "2º Ano de Informática",
+    quizzes: [
+      {
+        id: "2-quiz-1",
+        titulo: "",
+        perguntas: [
+          {
+            id: 1,
+            nivel: "Fácil",
+            pergunta: "",
+            alternativas: ["A)", "B)", "C)", "D)"],
+            respostaCorreta: 1
+          },
+          {
+            id: 2,
+            nivel: "Fácil",
+            pergunta: "",
+            alternativas: ["A)", "B)", "C)", "D)"],
+            respostaCorreta: 1
+          },
+          {
+            id: 3,
+            nivel: "Médio",
+            pergunta: "",
+            alternativas: ["A)",  "B)", "C)", "D)"],
+            respostaCorreta: 1
+          },
+          {
+            id: 4,
+            nivel: "Médio",
+            pergunta: "",
+            alternativas: ["A)",  "B)", "C)", "D)"],
+            respostaCorreta: 1
+          },
+          {
+            id: 5,
+            nivel: "Difícil",
+            pergunta: "",
+            alternativas: ["A)", "B)", "C)", "D)"],
+            respostaCorreta: 1
+          },
+          {
+            id: 6,
+            nivel: "Difícil",
+            pergunta: "",
+            alternativas: ["A)", "B)", "C)", "D)"],
+            respostaCorreta: 1
+          }
+        ]
+      },
+      {
+        id: "2-quiz-2",
+        titulo: "",
+        perguntas: [
+          {
+            id: 1,
+            nivel: "Fácil",
+            pergunta: "",
+            alternativas: ["A)", "B)", "C)", "D)"],
+            respostaCorreta: 1
+          },
+          {
+            id: 2,
+            nivel: "Fácil",
+            pergunta: "",
+            alternativas: ["A)", "B)", "C)", "D)"],
+            respostaCorreta: 1
+          },
+          {
+            id: 3,
+            nivel: "Médio",
+            pergunta: "",
+            alternativas: ["A)",  "B)", "C)", "D)"],
+            respostaCorreta: 1
+          },
+          {
+            id: 4,
+            nivel: "Médio",
+            pergunta: "",
+            alternativas: ["A)",  "B)", "C)", "D)"],
+            respostaCorreta: 1
+          },
+          {
+            id: 5,
+            nivel: "Difícil",
+            pergunta: "",
+            alternativas: ["A)", "B)", "C)", "D)"],
+            respostaCorreta: 1
+          },
+          {
+            id: 6,
+            nivel: "Difícil",
+            pergunta: "",
+            alternativas: ["A)", "B)", "C)", "D)"],
+            respostaCorreta: 1
+          }
+        ]
+      },
+      {
+        id: "2-quiz-3",
+        titulo: "",
+        perguntas: [
+          {
+            id: 1,
+            nivel: "Fácil",
+            pergunta: "",
+            alternativas: ["A)", "B)", "C)", "D)"],
+            respostaCorreta: 1
+          },
+          {
+            id: 2,
+            nivel: "Fácil",
+            pergunta: "",
+            alternativas: ["A)", "B)", "C)", "D)"],
+            respostaCorreta: 1
+          },
+          {
+            id: 3,
+            nivel: "Médio",
+            pergunta: "",
+            alternativas: ["A)",  "B)", "C)", "D)"],
+            respostaCorreta: 1
+          },
+          {
+            id: 4,
+            nivel: "Médio",
+            pergunta: "",
+            alternativas: ["A)",  "B)", "C)", "D)"],
+            respostaCorreta: 1
+          },
+          {
+            id: 5,
+            nivel: "Difícil",
+            pergunta: "",
+            alternativas: ["A)", "B)", "C)", "D)"],
+            respostaCorreta: 1
+          },
+          {
+            id: 6,
+            nivel: "Difícil",
+            pergunta: "",
+            alternativas: ["A)", "B)", "C)", "D)"],
+            respostaCorreta: 1
+          }
+        ]
+      }
+    ]
+  },
+
+  // ================= 3º ANO =================
+  {
+    ano: 3,
+    nome: "3º Ano de Informática",
+    quizzes: [
+      {
+        id: "3-quiz-1",
+        titulo: "",
+        perguntas: [
+          {
+            id: 1,
+            nivel: "Fácil",
+            pergunta: "",
+            alternativas: ["A)", "B)", "C)", "D)"],
+            respostaCorreta: 1
+          },
+          {
+            id: 2,
+            nivel: "Fácil",
+            pergunta: "",
+            alternativas: ["A)", "B)", "C)", "D)"],
+            respostaCorreta: 1
+          },
+          {
+            id: 3,
+            nivel: "Médio",
+            pergunta: "",
+            alternativas: ["A)",  "B)", "C)", "D)"],
+            respostaCorreta: 1
+          },
+          {
+            id: 4,
+            nivel: "Médio",
+            pergunta: "",
+            alternativas: ["A)",  "B)", "C)", "D)"],
+            respostaCorreta: 1
+          },
+          {
+            id: 5,
+            nivel: "Difícil",
+            pergunta: "",
+            alternativas: ["A)", "B)", "C)", "D)"],
+            respostaCorreta: 1
+          },
+          {
+            id: 6,
+            nivel: "Difícil",
+            pergunta: "",
+            alternativas: ["A)", "B)", "C)", "D)"],
+            respostaCorreta: 1
+          }
+        ]
+      },
+      {
+        id: "3-quiz-2",
+        titulo: "",
+        perguntas: [
+          {
+            id: 1,
+            nivel: "Fácil",
+            pergunta: "",
+            alternativas: ["A)", "B)", "C)", "D)"],
+            respostaCorreta: 1
+          },
+          {
+            id: 2,
+            nivel: "Fácil",
+            pergunta: "",
+            alternativas: ["A)", "B)", "C)", "D)"],
+            respostaCorreta: 1
+          },
+          {
+            id: 3,
+            nivel: "Médio",
+            pergunta: "",
+            alternativas: ["A)",  "B)", "C)", "D)"],
+            respostaCorreta: 1
+          },
+          {
+            id: 4,
+            nivel: "Médio",
+            pergunta: "",
+            alternativas: ["A)",  "B)", "C)", "D)"],
+            respostaCorreta: 1
+          },
+          {
+            id: 5,
+            nivel: "Difícil",
+            pergunta: "",
+            alternativas: ["A)", "B)", "C)", "D)"],
+            respostaCorreta: 1
+          },
+          {
+            id: 6,
+            nivel: "Difícil",
+            pergunta: "",
+            alternativas: ["A)", "B)", "C)", "D)"],
+            respostaCorreta: 1
+          }
+        ]
+      },
+      {
+        id: "3-quiz-3",
+        titulo: "",
+        perguntas: [
+          {
+            id: 1,
+            nivel: "Fácil",
+            pergunta: "",
+            alternativas: ["A)", "B)", "C)", "D)"],
+            respostaCorreta: 1
+          },
+          {
+            id: 2,
+            nivel: "Fácil",
+            pergunta: "",
+            alternativas: ["A)", "B)", "C)", "D)"],
+            respostaCorreta: 1
+          },
+          {
+            id: 3,
+            nivel: "Médio",
+            pergunta: "",
+            alternativas: ["A)",  "B)", "C)", "D)"],
+            respostaCorreta: 1
+          },
+          {
+            id: 4,
+            nivel: "Médio",
+            pergunta: "",
+            alternativas: ["A)",  "B)", "C)", "D)"],
+            respostaCorreta: 1
+          },
+          {
+            id: 5,
+            nivel: "Difícil",
+            pergunta: "",
+            alternativas: ["A)", "B)", "C)", "D)"],
+            respostaCorreta: 1
+          },
+          {
+            id: 6,
+            nivel: "Difícil",
+            pergunta: "",
+            alternativas: ["A)", "B)", "C)", "D)"],
+            respostaCorreta: 1
+          }
+        ]
+      }
+    ]
+  }
+];
+
+export {quizzesInfo};
+
+export default {quizzesInfo};
