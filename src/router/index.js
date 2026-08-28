8072e1bc0159d5e4a7eb5e89d5925ae133e7365e
camilamengarda <camila.machado.mg@gmail.com>
2026-08-27 22:49:38 -0300

    quiz agro, quiz quimi e ajustes no js pra encaixar na estrutura nova

--- src/router/index.js
+++ src/router/index.js
@@ -35,7 +35,7 @@ const router = createRouter({
       component: PaginaAtividades
     },
     {
-      path: '/quiz/:categoria/:numero',
+      path: '/quiz/:ano/:id',
       name: 'quiz',
       component: QuizView
     }
