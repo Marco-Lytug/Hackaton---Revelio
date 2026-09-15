ce3d266169cce3030a98d01a509dc3a8513cc1e1
RenatoBP100 <zerder101010@gmail.com>
2026-09-15 09:15:21 -0300

    começando o conteudo agro

--- src/Data/conteudoAgro.js
+++ src/Data/conteudoAgro.js
@@ -1,11 +1,39 @@
-const conteudoAgro1 = []
-const ConteudoAgro2 = []
-const ConteudoAgro3 = []
+const cursoAgro =[
+  {
+    id: 1,
+    ano: "primeiro ano",
+    materia: "Agropecuária 1",
+    descrição:"Curso de Agropecuária com formação voltada para o desenvolvimento de conhecimentos técnicos e práticos relacionados às atividades do setor agropecuário. Durante o curso, são abordados conteúdos relacionados à produção agrícola, manejo e criação de animais, conservação do solo, técnicas de cultivo, gestão e planejamento rural, além do uso de tecnologias aplicadas à produção. A formação também proporciona conhecimentos sobre sustentabilidade, preservação dos recursos naturais e boas práticas de produção, buscando unir produtividade, responsabilidade ambiental e eficiência no campo. O curso prepara o aluno para compreender e participar de diferentes etapas da produção agropecuária, contribuindo para o planejamento, execução e gerenciamento de atividades em propriedades rurais e demais áreas relacionadas ao agronegócio.",
+    duração:"30horas",
+    Modolos: [
+      {
+        idM:0,
+        titulo:"Introdução",
+        descriçãoM:"Uma introdução do nosso curso, o primeiro modolo dizendo o que contem em nosso curso sobre agropecuaria",
+        p1:"a",
+        img:"a",
+        p2:"",
+        img2:""
+      },
+      {
+        idM:1,
+        titulo: "Historia da Agrocultura",
+        descriçãoM: "A agricultura surgiu há cerca de 10 mil anos, quando os seres humanos começaram a cultivar plantas e domesticar animais, dando início à formação das primeiras comunidades. Esse processo transformou o modo de vida das sociedades, permitindo a produção de alimentos em maior quantidade e o desenvolvimento das primeiras civilizações. Com o passar do tempo, novas técnicas, ferramentas e conhecimentos foram desenvolvidos para melhorar a produção agrícola. A Revolução Industrial trouxe máquinas que facilitaram o trabalho no campo e aumentaram a produtividade. Atualmente, a agricultura conta com tecnologias modernas, como máquinas agrícolas, sistemas de irrigação, drones e agricultura de precisão. Além de produzir alimentos e matérias-primas, a agricultura moderna busca cada vez mais a sustentabilidade, a preservação do solo e da água e a redução dos impactos ambientais",
+        p1:"",
+        img:"",
+        p2:"",
+        img2:"",
+      },
+      {
+        idM:2,
+        descriçãoM:"a"
+      }
+    ]
 
-export { conteudoAgro1, ConteudoAgro2, ConteudoAgro3 }
+  },
+  {}
+]
 
-export default {
-  conteudAgro1,
-  ConteudoAgro2,
-  ConteudoAgro3
-}
+export { cursoAgro}
+
+export default cursoAgro
