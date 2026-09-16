1109ac02fcdd8651fc55c4674c6119b6dd1d9374
DaviMariano123 <masterday0000@gmail.com>
2026-09-16 16:28:08 -0300

    conflito

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,7 @@
-import { createClient } from '@supabase/supabase-js'
+ import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://supabase.co'
+const urlSupabase = 'https://supabase.co'
 
-const supabaseAnonKey = 'sb_publishable_p-YN04N8yT9P2R_tEY16Iw_b-gk1XbL' 
+const chaveAnonimaSupabase = 'sb_publishable_p-YN04N8yT9P2R_tEY16Iw_b-gk1XbL'
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const conexaoSupabase = createClient(urlSupabase, chaveAnonimaSupabase)
